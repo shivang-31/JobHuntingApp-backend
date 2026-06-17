bba7536751cfc27a1c7f7e1b68ac4cceaf58fa36
shivang-31 <shivangdixit844@gmail.com>
2026-06-18 00:58:56 +0530

    Fix CORS for Vercel frontend

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 import express from "express";
+import cors from "cors";
 import jobRoutes from "./routes/job.route.js";
+
 const app = express();
+
+// CORS
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://job-hunting-app-frontend-rho.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 // Middlewares
 app.use(express.json());
@@ -8,6 +21,5 @@ app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use("/api/jobs", jobRoutes);
-// app.use("/api/v1/users", userRouter);
 
 export { app };
